4e5171a88cb4b26b6b7f92723910e43ca303a047
BWillis98 <bwmusiclover1212@gmail.com>
2016-05-25 10:50:10 -0500

    Something went wrong initial upload

--- config.php
+++ config.php
@@ -1,7 +1,7 @@
 <?php
 $currency = '$';
 $db_username = 'root';
-$db_password = 'test123';
+$db_password = '';
 $db_name = 'sprinkleon';
 $db_host = 'localhost';
 $mysqli = new mysqli($db_host, $db_username, $db_password,$db_name);
