aeb990d1c1469a9056c99862b9cf4e12d0c4eb4d
Chetan <chetan_genius@yahoo.co.in>
2016-05-30 00:59:44 +0530

    paypal-button added

--- cart.php
+++ cart.php
@@ -4,7 +4,8 @@
 if(session_id() == '' || !isset($_SESSION)){session_start();}
 
 include 'config.php';
-
+include 'functions.php';
+include 'payments.php';
 
 ?>
 
@@ -134,7 +135,30 @@ include 'config.php';
           echo '</div>';
           ?>
 
+<hr>
+<div id ="paypal-text">
+<h4><i>Or pay with PayPal</i></h4>
+<form action="https://www.sandbox.paypal.com/cgi-bin/webscr" method="post">
 
+  <!-- Identify your business so that you can collect the payments. -->
+  <input type="hidden" name="business" value="herschelgomez@xyzzyu.com">
+
+  <!-- Specify a Buy Now button. -->
+  <input type="hidden" name="cmd" value="_xclick">
+
+  <!-- Specify details about the item that buyers will purchase. -->
+  <input type="hidden" name="item_name" value="Hot Sauce-12oz Bottle">
+  <input type="hidden" name="amount" value="5.95">
+  <input type="hidden" name="currency_code" value="USD">
+
+  <!-- Display the payment button. -->
+  <input type="image" name="submit" border="0"
+  src="https://www.paypalobjects.com/en_US/i/btn/btn_buynow_LG.gif"
+  alt="PayPal - The safer, easier way to pay online">
+  <img alt="" border="0" width="1" height="1"
+  src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" >
+
+</form>
 
     <div class="row" style="margin-top:10px;">
       <div class="small-12">
